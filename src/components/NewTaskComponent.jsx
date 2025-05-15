db16b28449e91ee130a9e53681909febe0bfebca
Riichan04 <22130274@st.hcmuaf.edu.vn>
2025-05-15 23:08:41 +0700

    Add icon for input title

--- src/components/NewTaskComponent.jsx
+++ src/components/NewTaskComponent.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
 import '../styles/task-table-component.css'
-import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import DriveFileRenameOutlineRoundedIcon from '@mui/icons-material/DriveFileRenameOutlineRounded';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-import { smallButtonStyle, textButtonStyle } from "../themes/componentStyling";
+import SubjectRoundedIcon from '@mui/icons-material/SubjectRounded';
+import EventRoundedIcon from '@mui/icons-material/EventRounded';
+import { smallButtonStyle } from "../themes/componentStyling";
 
 const TaskTableComponent = ({ displayState }) => {
     const [display, setDisplay] = useState(displayState)
@@ -17,15 +19,26 @@ const TaskTableComponent = ({ displayState }) => {
             {display ? (
                 <div id="task-table">
                     <div id="task-table--header">
-                        <h4 style={{margin: 'auto 0'}}>Task Table</h4>
+                        <h4 style={{margin: 'auto 0', fontWeight: '500'}}>Add a new task</h4>
                         <CloseRoundedIcon sx={smallButtonStyle} onClick={() => closePopup(false)}/>
                     </div>
                     <div>
-                        <p>Name</p>
+                        <div>
+                            <DriveFileRenameOutlineRoundedIcon sx={{color: 'var(--text-color)'}}/>
+                            <span>Name</span>
+                        </div>
                         <input type="text" name="" id="" />
-                        <p>Description</p>
+
+                        <div>
+                            <SubjectRoundedIcon sx={{color: 'var(--text-color)'}}/>
+                            <span>Description</span>
+                        </div>
                         <textarea name="" id="" style={{ resize: 'none' }} rows={10}></textarea>
-                        <p>Deadline</p>
+
+                        <div>
+                            <EventRoundedIcon sx={{color: 'var(--text-color)'}}/>
+                            <span>Deadline</span>
+                        </div>
                         <input type="date" name="" id="" />
                     </div>
                     <div style={{display: 'flex', justifyContent: 'end', marginTop: '2rem', width: '100%'}}>
