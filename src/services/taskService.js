4e1a197b3074aef36a9f304ca84d209ffbf92ea8
Riichan04 <22130274@st.hcmuaf.edu.vn>
2025-05-24 01:51:31 +0700

    Add new task detail to send to server

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -2,8 +2,8 @@ import { sendNewTaskData } from "../api/taskApi"
 
 //5.1.1.4. Kiểm tra lại thông tin đã nhập
 function validateNewTaskData(taskData) {
-    if (Date.now() - taskData.deadline < 5*60*1000 ||
-        taskData.name === ""|| isTaskExist(taskData.name))
+    if (Date.now() - taskData.endTime < 5*60*1000 ||
+        taskData.title === ""|| isTaskExist(taskData.title))
         //Temp code
         sendNewTaskData(taskData)
     else return null
